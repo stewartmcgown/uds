969cd71fd8d91ae4d0b15397d169f24c756a05fc
Stewart McGown <stewartcallummcgown@gmail.com>
2018-12-04 23:10:59 +0000

    Was never gonna encrypt anything anyway :disappointed_relieved:

--- Encoder.py
+++ Encoder.py
@@ -2,19 +2,6 @@ import cryptography
 import os
 import base64
 
-def encrypt(chunk):
-    backend = default_backend()
-    key = os.urandom(32)
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    encryptor = cipher.encryptor()
-    ct = encryptor.update(b"a secret message") + encryptor.finalize()
-    return ct
-
-def decrypt(chunk):
-    decryptor = cipher.decryptor()
-    decryptor.update(ct) + decryptor.finalize()
-
 def encode(chunk):
     enc = base64.b64encode(chunk).decode()
     return enc
