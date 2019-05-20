934d5ec7c63e518d8b690691d7917a4c467a725e
Sammy Hass <me@sammyhass.io>
2019-05-20 13:54:13 +0100

    More Stable Exception Handling and PEP8 Fixes
    Signed-off-by: Sammy Hass <me@sammyhass.io>

---
+++ custom_exceptions.py
@@ -0,0 +1,85 @@
+"""File containing custom rutmine exceptions"""
+import sys
+
+
+class Error(Exception):
+    """Base class for exceptions in this module"""
+    def __init__(self):
+        pass
+
+    def formatter(self, instance=None, exception_has=None):
+        try:
+            if exception_has:
+                raise(instance(exception_has))
+            else:
+                raise(instance())
+        except instance as err:
+            print("\nFailed with: {}\n".format(type(err).__name__))
+            print(err)
+        finally:
+            sys.exit()
+            print(" ")
+    
+
+class FileNotUDSError(Error):
+    """Raises when file is found, but it is not of type uds
+
+    Attributes:
+        message -- explanation of why exception was raised. 
+
+    """
+
+    def __init__(self, message=None):
+        self.message = message
+
+    def __str__(self):
+
+        return "{}".format(self.message)
+
+class PythonVersionError(Error):
+
+
+    """Raised when this a script is run with an unsupported Python version (<=3.0.0).
+    
+    Attributes:
+        version_used -- the python version that caught the error.
+        command_used -- the full command used to run this script.
+        message      -- explanation of why exception was raised.
+
+    """
+    def __init__(self, version_used):
+        self.version_used = version_used
+        self.message = "Attempted to run the module with Python version of {}, which is not supported by this module\nPlease use Python 3 (or higher) instead.".format(self.version_used)
+
+    def formatter(self):
+        super(PythonVersionError, self).formatter(self.get_instance(), self.version_used)
+
+    def get_instance(self):
+        return PythonVersionError
+
+    def __str__(self):
+        return "{}".format(self.message)
+
+class NoClientSecretError(Error):
+    """Raised when the current working directory has no file name 'client_secret.json'
+
+    If this error is raised, be sure you renamed 'credentials.json' to 'client_secret.json'
+
+    Attributes:
+        message  -- explanation why exception was reaised.
+
+    """
+    def __init__(self):
+        self.message = "No file named 'client_secret.json' in current working directory. Ensure you've followed the installation instructions on GitHub."
+
+    def formatter(self):
+        super().formatter(self.get_instance())
+
+    def get_instance(self):
+        return NoClientSecretError
+
+    def __str__(self):
+        return "{}".format(self.message)
+
+
+
--- size_formatting.py
+++ size_formatting.py
@@ -1,4 +1,4 @@
-def format(number_of_bytes, numeric=False):
+def formatter(number_of_bytes, numeric=False):
     if number_of_bytes < 0:
         raise ValueError("!!! number_of_bytes can't be smaller than 0 !!!")
 
